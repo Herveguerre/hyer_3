6651a90a952baff6de5773d3d707f9da402ab3a4
herve guerre <herveguerre@hotmail.fr>
2023-07-19 13:54:18 +0200

    m

---
+++ src/components/volspartagesPage/Derniervolsajoutes.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Derniervolsajoutes() {
+  return (
+    <div>Derniervolsajoutes</div>
+  )
+}
---
+++ src/pages/Propose_a_flight.js
@@ -0,0 +1,12 @@
+import VolsartpartagesHeader from '@/components/volspartagesPage/VolsartpartagesHeader'
+import Derniervolsajoutes from '@/components/volspartagesPage/Derniervolsajoutes'
+import React from 'react'
+
+export default function Propose_a_flight() {
+  return (
+    <div>
+        <VolsartpartagesHeader/>
+        <Derniervolsajoutes/>
+    </div>
+  )
+}

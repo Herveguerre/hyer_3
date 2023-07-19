43ced0b1b4078a828a12c29193b14388f8211251
herve guerre <herveguerre@hotmail.fr>
2023-07-19 17:07:41 +0200

    m

--- src/components/volspartagesPage/Derniervolsajoutes.js
+++ src/components/volspartagesPage/Derniervolsajoutes.js
@@ -2,6 +2,22 @@ import React from 'react'
 
 export default function Derniervolsajoutes() {
   return (
-    <div>Derniervolsajoutes</div>
+    <div className='marge'>
+      <div className='space w1440'>
+        <div className='flex'>
+          <p>Derniers vols ajoutés</p> 
+          <button>Recherche de vols</button>
+        </div>
+        <div className='tableau'>
+          <div className='noir flex'>
+            <p>Farnborrough Sion</p><p>Sens unique</p><p>Proposé*</p>
+          </div>
+          <div className='gris flex'>
+            <p>Farnborrough Sion</p><p>Sortant</p>
+          </div>
+
+        </div>
+      </div>
+    </div>
   )
 }

dbe68661c67f57729e27358f8f0a133c2bec36d4
herve guerre <herveguerre@hotmail.fr>
2023-08-27 15:24:37 +0200

    m

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,16 +1,19 @@
 import Footer from '@/components/Footer'
+import Header from '@/components/Header'
 import AboutHeader from '@/components/aboutPage/AboutHeader'
 import AboutSection from '@/components/aboutPage/AboutSection'
 import AvisClients from '@/components/aboutPage/AvisClients'
 import EquipeFondatrice from '@/components/aboutPage/EquipeFondatrice'
 import NotreTechnologie from '@/components/aboutPage/NotreTechnologie'
 import PourquoiHyer from '@/components/aboutPage/PourquoiHyer'
+import HomeHero from '@/components/element_home/HomeHero'
 import React from 'react'
 
 export default function About() {
   return (
     <div>
-        <AboutHeader/>
+        <Header/>
+        
         <AboutSection/>
         <EquipeFondatrice/>
         <PourquoiHyer/>
